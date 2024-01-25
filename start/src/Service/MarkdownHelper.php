37c0c5c8f9f6626e7190c2f07850ef4a7de36db3
mgnhbnr <mh@creavo.de>
2024-01-25 14:58:20 +0100

    accessing logging service

--- start/src/Service/MarkdownHelper.php
+++ start/src/Service/MarkdownHelper.php
@@ -3,23 +3,36 @@
 namespace App\Service;
 
 use Knp\Bundle\MarkdownBundle\MarkdownParserInterface;
+use Psr\Log\LoggerInterface;
 use Symfony\Contracts\Cache\CacheInterface;
 
 class MarkdownHelper {
 
     private $markdownParser;
     private $cache;
+    private $isDebug;
+    private $logger;
 
-    public function __construct(MarkdownParserInterface $markdownParser, CacheInterface $cache) {
+    public function __construct(MarkdownParserInterface $markdownParser, CacheInterface $cache, bool $isDebug, LoggerInterface $mdLogger) {
         $this -> markdownParser = $markdownParser;
         $this -> cache = $cache;
+        $this -> isDebug = $isDebug;
+        $this -> logger = $mdLogger;
     }
 
     public function parse(string $source){
+
+        if(stripos($source,'cat') !== false) {
+            $this -> logger -> info('miau');
+        }
+
+        if($this -> isDebug) {
+            return $this -> markdownParser -> transformMarkdown($source);
+        }
+
         return $this->cache->get('markdown_'.md5($source), function() use($source) {
 
             return $this -> markdownParser -> transformMarkdown($source);
-
         });
     }
 }
